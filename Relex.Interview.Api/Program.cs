264ac34e20bddad77fd656251a23c2b4f21bb856
shahrzad jahanbaz <shahrzad@mobilityonegroup.com>
2022-06-24 13:42:43 +0300

    add swagger ui in production!

--- Relex.Interview.Api/Program.cs
+++ Relex.Interview.Api/Program.cs
@@ -22,12 +22,8 @@ builder.Services.AddTransient(typeof(IProductBatchRepository), typeof(ProductBat
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger(); 
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
